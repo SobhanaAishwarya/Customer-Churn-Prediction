6a7302ebbca91cab8a12035d95537025dfdb80ff
Aishwarya Nath <aishwaryasobhana@gmail.com>
2026-05-13 14:49:49 +0530

    Updated app

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Customer Churn Prediction")
+st.title("Customer Churn Prediction System")
 
-st.write("My ML Project is Running Successfully!")
+df = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
+
+st.subheader("Dataset Preview")
+st.write(df.head())
+
+st.subheader("Dataset Shape")
+st.write(df.shape)
+
+st.subheader("Columns")
+st.write(df.columns)
